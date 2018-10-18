4e0a66ca7681d9680a457d2b09faeca03d006274
Paul Maddox <pmaddox@amazon.com>
2018-10-18 22:16:42 +0300

    Added DNS, CloudFront, S3 bucket and a sprinkle of GuardDuty

--- bin/cdkworkshop.com.ts
+++ bin/cdkworkshop.com.ts
@@ -1,23 +1,89 @@
 #!/usr/bin/env node
 import cdk = require('@aws-cdk/cdk');
 import route53 = require('@aws-cdk/aws-route53');
+import cloudfront = require('@aws-cdk/aws-cloudfront');
+import s3 = require('@aws-cdk/aws-s3');
+import { GuardDutyNotifier } from './guardduty';
 
 class CdkWorkshop extends cdk.Stack {
+
+    public readonly domain = 'cdkworkshop.com';
+    public readonly certificate = 'arn:aws:acm:us-east-1:025656461920:certificate/c75d7a9d-1253-4506-bc6d-5874767b3c35';
+    public readonly email = 'aws-cdk-workshop@amazon.com';
+
     constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
         super(parent, name, props);
 
-        const zone = new route53.PublicHostedZone(this, "HostedZone", {
-            zoneName: "cdkworkshop.com",
+        // Enable AWS GuardDuty in this account, and send any security findings via email
+        new GuardDutyNotifier(this, "GuardDuty", {
+            environmentName: this.domain,
+            email: this.email
         })
 
-        new cdk.Output(this, "DNSNameservers", {
-            description: "Nameservers for DNS zone",
-            value: zone.nameServers,
+        // Create DNS Zone
+        const zone = new route53.PublicHostedZone(this, 'HostedZone', {
+            zoneName: this.domain,
+        })
+
+        // Bucket to hold the static website
+        const bucket = new s3.Bucket(this, 'Bucket');
+
+        // TODO: Create BucketDeployment for syncing workshop/public/* up to S3 once construct is available 
+
+        // CloudFront distribution
+        const cdn = new cloudfront.CloudFrontWebDistribution(this, 'CloudFront', {
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.RedirectToHTTPS,
+            priceClass: cloudfront.PriceClass.PriceClassAll,
+            originConfigs: [{
+                behaviors: [{ isDefaultBehavior: true }],
+                s3OriginSource: {
+                    s3BucketSource: bucket,
+                }
+            }],
+            aliasConfiguration: {
+                names: [this.domain],
+                acmCertRef: this.certificate,
+            }
+        })
+
+        // DNS alias for the CloudFront distribution
+        // Having to use L1 construct here as currently only TXT/NS records can be created with the L2 construct
+        // https://github.com/awslabs/aws-cdk/issues/966
+        new route53.cloudformation.RecordSetResource(this, 'CloudFrontDNSRecord', {
+            recordSetName: this.domain + '.',
+            hostedZoneId: zone.hostedZoneId,
+            type: 'A',
+            aliasTarget: {
+                hostedZoneId: 'Z2FDTNDATAQYW2', // Always used for CloudFront in any region
+                dnsName: cdn.domainName,
+            },
+        });
+
+        // Configure Outputs
+
+        new cdk.Output(this, 'URL', {
+            description: 'The URL of the workshop',
+            value: 'https://' + this.domain,
+        })
+
+        new cdk.Output(this, 'CloudFrontURL', {
+            description: 'The CloudFront distribution URL',
+            value: 'https://' + cdn.domainName,
+        })
+
+        new cdk.Output(this, 'CertificateArn', {
+            description: 'The SSL certificate ARN',
+            value: this.certificate,
+        })
+
+        new cdk.Output(this, 'Nameservers', {
+            description: 'Nameservers for DNS zone',
+            value: zone.nameServers.concat("", ","),
         })
 
     }
 }
 
 const app = new cdk.App();
-new CdkWorkshop(app, 'CDK-WORKSHOP-PROD', { env: { account: "025656461920", region: "eu-west-1" } });
+new CdkWorkshop(app, 'CDK-WORKSHOP-PROD', { env: { account: '025656461920', region: 'eu-west-1' } });
 app.run();
---
+++ bin/guardduty.ts
@@ -0,0 +1,42 @@
+import cdk = require('@aws-cdk/cdk');
+import guardduty = require('@aws-cdk/aws-guardduty');
+import events = require('@aws-cdk/aws-events');
+import sns = require('@aws-cdk/aws-sns');
+
+export class GuardDutyNotifierProps {
+
+    // An environment name included in the notification email for identification purposes
+    environmentName: string
+
+    // The email address to send security findings to
+    email: string
+}
+
+export class GuardDutyNotifier extends cdk.Construct {
+    constructor(parent: cdk.Stack, name: string, props: GuardDutyNotifierProps) {
+        super(parent, name);
+
+        // Enable GuardDuty in the AWS region, to detect security issues
+        new guardduty.cloudformation.DetectorResource(this, "GuardDutyDetector", { enable: true })
+
+        // Configure GuardDuty to email any security findings
+        const guardDutyTopic = new sns.Topic(this, "GuardDutyNotificationTopic");
+        guardDutyTopic.subscribeEmail("GuardDutyNotificationSubscription", props.email);
+        const eventRule = new events.EventRule(this, "GuardDutyEventRule", {
+            eventPattern: {
+                source: ["aws.guardduty"],
+                detailType: ["GuardDuty Finding"],
+            }
+        })
+
+        // Format the GuardDuty findings emails (rather than just a large splodge of JSON)
+        eventRule.addTarget(guardDutyTopic, {
+            pathsMap: {
+                "region": "$.region",
+                "type": "$.detail.type",
+            },
+            textTemplate: "WARNING: AWS GuardDuty has discovered a <type> security issue for " + props.environmentName + " (<region>). Please go to https://<region>.console.aws.amazon.com/guardduty/ to find out more details.",
+        })
+
+    }
+}
