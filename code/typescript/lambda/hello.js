cf063dfbc9dc1c3fb44fa4e4d09f1af2c5a46272
Elad Ben-Israel <benisrae@amazon.com>
2019-03-21 11:20:34 +0200

    Modify hello response to check CI/CD

--- code/typescript/lambda/hello.js
+++ code/typescript/lambda/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function(event) {
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${event.path}\n`
+    body: `Hello, CDK! You've hit "${event.path}". That's not very polite, "${event.path}" did not do anything to you...\n`
   };
 };
