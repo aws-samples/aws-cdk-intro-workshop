554433eb6b67ef492959504ae8f2f519a2e6c039
Gábor Lipták <gliptak@gmail.com>
2021-07-22 13:35:29 -0400

    fix: Make Python main-workshop deployable (#240)
    Signed-off-by: Gábor Lipták <gliptak@gmail.com>
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- code/python/main-workshop/setup.py
+++ code/python/main-workshop/setup.py
@@ -15,11 +15,14 @@ setuptools.setup(
 
     author="author",
 
-    package_dir={"": "cdk-workshop"},
+    package_dir={"": "cdk_workshop"},
     packages=setuptools.find_packages(where="cdk-workshop"),
 
     install_requires=[
-        "aws-cdk.core==1.71.0",
+        "aws-cdk.core==1.106.0",
+        "aws-cdk.aws_lambda==1.106.0",
+        "aws-cdk.aws_apigateway==1.106.0",
+        "cdk-dynamo-table-view==0.1.19",
     ],
 
     python_requires=">=3.6",
