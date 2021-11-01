cafaa1d83e091e7d7f3005f8e8e38370bd45a3eb
Cory Hall <43035978+corymhall@users.noreply.github.com>
2021-11-01 11:03:50 -0400

    feat(testing): add tests-workshop code for java and python (#312)
    adding tests-workshop code for java and python using the new assertions
    library. Also updates the existing typescript code to use the assertions
    library.

---
+++ code/python/tests-workshop/tests/unit/test_cdk_workshop.py
@@ -0,0 +1,69 @@
+from aws_cdk import (
+        core,
+        aws_lambda as _lambda,
+        assertions
+    )
+from cdk_workshop.hitcounter import HitCounter
+import pytest
+
+
+def test_dynamodb_table_created():
+    stack = core.Stack()
+    HitCounter(stack, "HitCounter",
+            downstream=_lambda.Function(stack, "TestFunction",
+                runtime=_lambda.Runtime.NODEJS_14_X,
+                handler='hello.handler',
+                code=_lambda.Code.from_asset('lambda')),
+    )
+    template = assertions.Template.from_stack(stack)
+    template.resource_count_is("AWS::DynamoDB::Table", 1)
+
+
+def test_lambda_has_env_vars():
+    stack = core.Stack()
+    HitCounter(stack, "HitCounter",
+            downstream=_lambda.Function(stack, "TestFunction",
+                runtime=_lambda.Runtime.NODEJS_14_X,
+                handler='hello.handler',
+                code=_lambda.Code.from_asset('lambda')))
+    template = assertions.Template.from_stack(stack)
+    envCapture = assertions.Capture()
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "Handler": "hitcount.handler",
+        "Environment": envCapture,
+        })
+    assert envCapture.as_object() == {
+            "Variables": {
+                "DOWNSTREAM_FUNCTION_NAME": {"Ref": "TestFunction22AD90FC"},
+                "HITS_TABLE_NAME": {"Ref": "HitCounterHits079767E5"},
+                },
+            }
+
+
+def test_dynamodb_with_encryption():
+    stack = core.Stack()
+    HitCounter(stack, "HitCounter",
+            downstream=_lambda.Function(stack, "TestFunction",
+                runtime=_lambda.Runtime.NODEJS_14_X,
+                handler='hello.handler',
+                code=_lambda.Code.from_asset('lambda')))
+    template = assertions.Template.from_stack(stack)
+    template.has_resource_properties("AWS::DynamoDB::Table", {
+        "SSESpecification": {
+            "SSEEnabled": True,
+            },
+        })
+
+
+def test_dynamodb_raises():
+    stack = core.Stack()
+    with pytest.raises(Exception):
+        HitCounter(stack, "HitCounter",
+                downstream=_lambda.Function(stack, "TestFunction",
+                    runtime=_lambda.Runtime.NODEJS_14_X,
+                    handler='hello.handler',
+                    code=_lambda.Code.from_asset('lambda')),
+                read_capacity=1,
+                )
+
+
--- code/typescript/tests-workshop/test/hitcounter.test.ts
+++ code/typescript/tests-workshop/test/hitcounter.test.ts
@@ -1,76 +1,85 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { Template, Capture } from '@aws-cdk/assertions';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { HitCounter } from '../lib/hitcounter';
 
 test('DynamoDB Table Created', () => {
-    const stack = new cdk.Stack();
+  const stack = new cdk.Stack();
 
-    new HitCounter(stack, 'MyTestConstruct', {
-        downstream: new lambda.Function(stack, 'TestFunction', {
-            runtime: lambda.Runtime.NODEJS_14_X,
-            handler: 'hello.handler',
-            code: lambda.Code.fromAsset('lambda')
-        })
-    });
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
 
-    expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs("AWS::DynamoDB::Table", 1);
 });
 
 test('Lambda Has Environment Variables', () => {
-    const stack = new cdk.Stack();
+  const stack = new cdk.Stack();
 
-    new HitCounter(stack, 'MyTestConstruct', {
-        downstream: new lambda.Function(stack, 'TestFunction', {
-            runtime: lambda.Runtime.NODEJS_14_X,
-            handler: 'hello.handler',
-            code: lambda.Code.fromAsset('lambda')
-        })
-    });
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
 
-    expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
-        Environment: {
-            Variables: {
-                DOWNSTREAM_FUNCTION_NAME: {
-                    "Ref": "TestFunction22AD90FC"
-                },
-                HITS_TABLE_NAME: {
-                    "Ref": "MyTestConstructHits24A357F0"
-                }
-            }
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Handler: 'hitcounter.handler',
+    Environment: envCapture,
+  });
+  expect(envCapture.asObject()).toEqual(
+    {
+      Variables: {
+        DOWNSTREAM_FUNCTION_NAME: {
+          "Ref": "TestFunction22AD90FC"
+        },
+        HITS_TABLE_NAME: {
+          "Ref": "MyTestConstructHits24A357F0"
         }
-    }));
+      },
+    }
+  );
 });
 
 test('DynamoDB Table Created With Encryption', () => {
-    const stack = new cdk.Stack();
+  const stack = new cdk.Stack();
 
-    new HitCounter(stack, 'MyTestConstruct', {
-        downstream: new lambda.Function(stack, 'TestFunction', {
-            runtime: lambda.Runtime.NODEJS_14_X,
-            handler: 'hello.handler',
-            code: lambda.Code.fromAsset('lambda')
-        })
-    });
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
 
-    expectCDK(stack).to(haveResource("AWS::DynamoDB::Table", {
-        SSESpecification: {
-            SSEEnabled: true
-        }
-    }));
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  });
 });
 
 test('Read Capacity can be configured', () => {
-    const stack = new cdk.Stack();
+  const stack = new cdk.Stack();
 
-    expect(() => {
-        new HitCounter(stack, 'MyTestConstruct', {
-            downstream: new lambda.Function(stack, 'TestFunction', {
-                runtime: lambda.Runtime.NODEJS_14_X,
-                handler: 'hello.handler',
-                code: lambda.Code.fromAsset('lambda')
-            }),
-            readCapacity: 3
-        });
-    }).toThrowError("readCapacity must be greater than 5 and less than 20");
+  expect(() => {
+    new HitCounter(stack, 'MyTestConstruct', {
+      downstream: new lambda.Function(stack, 'TestFunction', {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'hello.handler',
+        code: lambda.Code.fromAsset('lambda')
+      }),
+      readCapacity: 3
+    });
+  }).toThrowError("readCapacity must be greater than 5 and less than 20");
 });
