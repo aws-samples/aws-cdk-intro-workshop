8b48eadbc657bc966165302d48aaf03d9ca8651b
Tom Keller <kellertk@amazon.com>
2022-08-26 19:56:18 -0700

    fix origin access identity

--- cdkworkshop.com/cdkworkshop.com.ts
+++ cdkworkshop.com/cdkworkshop.com.ts
@@ -7,7 +7,13 @@ import {
     aws_s3 as s3,
     aws_s3_deployment as s3deploy,
     aws_cloudfront_origins as cloudfrontOrigins,
-    App, CfnOutput, Duration, Fn, Stack, StackProps, Stage,
+    App,
+    CfnOutput,
+    Duration,
+    Fn,
+    Stack,
+    StackProps,
+    Stage,
 } from 'aws-cdk-lib';
 import { GuardDutyNotifier } from './guardduty';
 import path = require('path');
@@ -16,32 +22,31 @@ import { PipelineStack } from './pipeline';
 import { Construct } from 'constructs';
 
 export interface CdkWorkshopProps extends StackProps {
-
     /**
      * The Domain the workshop should be hosted at
      */
-    domain: string
+    domain: string;
 
     /**
      * The ARN of the Amazon Certificate Manager (ACM) certificate to use with CloudFront
      */
-    certificate: string
+    certificate: string;
 
     /**
      * Email address to use for AWS GuardDuty security finding notifications
      */
-    email: string
+    email: string;
 
     /**
      * If true, AWS WAF will be deployed in front of the workshop CloudFront
      * distribution, with a ruleset to only allow access from the Amazon corporate network
      */
-    restrictToAmazonNetwork: boolean
+    restrictToAmazonNetwork: boolean;
 
     /**
      * The ARN of the AWS WAF WebACL that restricts access to the Amazon corporate network (should be deployed separately)
      */
-    restrictToAmazonNetworkWebACL: string
+    restrictToAmazonNetworkWebACL: string;
 
     /**
      * If enabled, sets the max TTL to 0 in the CloudFront distribution.
@@ -51,32 +56,23 @@ export interface CdkWorkshopProps extends StackProps {
 }
 
 export class CdkWorkshop extends Stack {
-
     constructor(scope: Construct, id: string, props: CdkWorkshopProps) {
         super(scope, id, props);
 
         this.renameLogicalId('CloudFrontDNSRecord46217411', 'CloudFrontDNSRecord');
 
         // Enable AWS GuardDuty in this account, and send any security findings via email
-        new GuardDutyNotifier(this, "GuardDuty", {
+        new GuardDutyNotifier(this, 'GuardDuty', {
             environmentName: props.domain,
-            email: props.email
-        })
+            email: props.email,
+        });
 
         // Create DNS Zone
         const zone = new route53.PublicHostedZone(this, 'HostedZone', {
             zoneName: props.domain,
-        })
-
+        });
         // Bucket to hold the static website
-        const bucket = new s3.Bucket(this, 'Bucket', {
-            websiteIndexDocument: 'index.html'
-        });
-
-        const origin = new cloudfront.OriginAccessIdentity(this, "BucketOrigin", {
-            comment: props.domain,
-        });
-        (origin.node.defaultChild as cloudfront.CfnCloudFrontOriginAccessIdentity).overrideLogicalId('BucketOrigin');
+        const bucket = new s3.Bucket(this, 'Bucket', {});
 
         // Due to a bug in `BucketDeployment` (awslabs/aws-cdk#981) we must
         // deploy each version of the content to a different prefix (it's also a
@@ -84,19 +80,23 @@ export class CdkWorkshop extends Stack {
         // `BucketDeployment`).
         const contentDir = path.join(__dirname, '..', 'workshop', 'public');
         const contentHash = hashDirectorySync(contentDir);
-
         new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-            sources: [
-                s3deploy.Source.asset(contentDir)
-            ],
+            sources: [s3deploy.Source.asset(contentDir)],
             destinationBucket: bucket,
             destinationKeyPrefix: contentHash,
-            retainOnDelete: true
+            retainOnDelete: true,
         });
 
-        let acl: string | undefined
+        const originAccess = new cloudfront.OriginAccessIdentity(this, 'BucketOrigin', {
+            comment: props.domain,
+        });
+        (originAccess.node.defaultChild as cloudfront.CfnCloudFrontOriginAccessIdentity).overrideLogicalId(
+            'BucketOrigin',
+        );
+
+        let acl: string | undefined;
         if (props.restrictToAmazonNetwork) {
-            acl = props.restrictToAmazonNetworkWebACL.toString()
+            acl = props.restrictToAmazonNetworkWebACL.toString();
         }
 
         const maxTtl = props.disableCache ? Duration.seconds(0) : undefined;
@@ -105,9 +105,11 @@ export class CdkWorkshop extends Stack {
         const cert = acm.Certificate.fromCertificateArn(this, 'Certificate', props.certificate);
         const cdn = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
             defaultBehavior: {
+                allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
                 viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
                 origin: new cloudfrontOrigins.S3Origin(bucket, {
-                    originAccessIdentity: origin, originPath: `/${contentHash}`
+                    originAccessIdentity: originAccess,
+                    originPath: `/${contentHash}`,
                 }),
                 cachePolicy: new cloudfront.CachePolicy(this, 'CachePolicy', {
                     maxTtl,
@@ -117,17 +119,24 @@ export class CdkWorkshop extends Stack {
                         frameOptions: { frameOption: cloudfront.HeadersFrameOption.DENY, override: true },
                         contentTypeOptions: { override: true },
                         xssProtection: { protection: true, modeBlock: true, override: true },
-                        referrerPolicy: { referrerPolicy: cloudfront.HeadersReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN, override: true },
-                        strictTransportSecurity: { accessControlMaxAge: Duration.seconds(31536000), includeSubdomains: true, override: true },
-                    }
+                        referrerPolicy: {
+                            referrerPolicy: cloudfront.HeadersReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN,
+                            override: true,
+                        },
+                        strictTransportSecurity: {
+                            accessControlMaxAge: Duration.seconds(31536000),
+                            includeSubdomains: true,
+                            override: true,
+                        },
+                    },
                 }),
             },
+            defaultRootObject: 'index.html',
             webAclId: acl,
             priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
             certificate: cert,
-            domainNames: [ props.domain ],
+            domainNames: [props.domain],
         });
-
         (cdn.node.defaultChild as cloudfront.CfnDistribution).overrideLogicalId('CloudFrontCFDistribution57EFBAC6');
 
         // TODO: Model dependency from CloudFront Web Distribution on S3 Bucket Deployment
@@ -140,49 +149,45 @@ export class CdkWorkshop extends Stack {
         });
 
         // Email security records
-        const emailRecordComment = 'This record restricts email functionality to help prevent spoofing and impersonation events.';
+        const emailRecordComment =
+            'This record restricts email functionality to help prevent spoofing and impersonation events.';
         new route53.TxtRecord(this, 'EmailSpfRecord', {
             zone,
             values: ['v=spf1 -all'],
             comment: emailRecordComment,
-        })
+        });
         new route53.TxtRecord(this, 'EmailDkimRecord', {
-            recordName: "*._domainkey",
+            recordName: '*._domainkey',
             zone,
             values: ['v=DKIM1; p='],
             comment: emailRecordComment,
-        })
+        });
         new route53.TxtRecord(this, 'EmailDmarcRecord', {
-            recordName: "_dmarc",
+            recordName: '_dmarc',
             zone,
             values: ['v=DMARC1; p=reject; rua=mailto:report@dmarc.amazon.com; ruf=mailto:report@dmarc.amazon.com'],
             comment: emailRecordComment,
-        })
+        });
 
         // Configure Outputs
-
         new CfnOutput(this, 'URL', {
             description: 'The URL of the workshop',
             value: 'https://' + props.domain,
-        })
-
+        });
         new CfnOutput(this, 'CloudFrontURL', {
             description: 'The CloudFront distribution URL',
             value: 'https://' + cdn.distributionDomainName,
-        })
-
+        });
         new CfnOutput(this, 'CertificateArn', {
             description: 'The SSL certificate ARN',
             value: props.certificate,
-        })
-
+        });
         if (zone.hostedZoneNameServers) {
             new CfnOutput(this, 'Nameservers', {
                 description: 'Nameservers for DNS zone',
-                value: Fn.join(', ', zone.hostedZoneNameServers)
-            })
+                value: Fn.join(', ', zone.hostedZoneNameServers),
+            });
         }
-
     }
 }
 
@@ -199,7 +204,7 @@ export class TheCdkWorkshopStage extends Stage {
             email: 'aws-cdk-workshop@amazon.com',
             restrictToAmazonNetwork: false,
             restrictToAmazonNetworkWebACL: Fn.importValue('AMAZON-CORP-NETWORK-ACL:AmazonNetworkACL'),
-            disableCache: true
+            disableCache: true,
         });
     }
 }
