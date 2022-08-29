f997acfc027f62634ae79af3d1e496daecdac965
Tom Keller <1083460+kellertk@users.noreply.github.com>
2022-08-29 13:47:26 -0700

    feat: migrate cf distribution new construct (#643)
    - Migrated the distribution to the new Distribution construct
    - Added security headers to client responses
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [Apache 2.0 license].
    
    [Apache 2.0 license]: https://www.apache.org/licenses/LICENSE-2.0

--- cdkworkshop.com/guardduty.ts
+++ cdkworkshop.com/guardduty.ts
@@ -9,12 +9,11 @@ import {
 import { Construct } from 'constructs';
 
 export interface GuardDutyNotifierProps {
-
     // An environment name included in the notification email for identification purposes
-    environmentName: string
+    environmentName: string;
 
     // The email address to send security findings to
-    email: string
+    email: string;
 }
 
 export class GuardDutyNotifier extends Construct {
@@ -22,22 +21,31 @@ export class GuardDutyNotifier extends Construct {
         super(scope, id);
 
         // Enable GuardDuty in the AWS region, to detect security issues
-        new guardduty.CfnDetector(this, "GuardDutyDetector", { enable: true })
+        new guardduty.CfnDetector(this, 'GuardDutyDetector', { enable: true });
 
         // Configure GuardDuty to email any security findings
-        const guardDutyTopic = new sns.Topic(this, "GuardDutyNotificationTopic");
+        const guardDutyTopic = new sns.Topic(this, 'GuardDutyNotificationTopic');
         guardDutyTopic.addSubscription(new subs.EmailSubscription(props.email));
-        const eventRule = new events.Rule(this, "GuardDutyEventRule", {
+        const eventRule = new events.Rule(this, 'GuardDutyEventRule', {
             eventPattern: {
-                source: ["aws.guardduty"],
-                detailType: ["GuardDuty Finding"],
-            }
-        })
+                source: ['aws.guardduty'],
+                detailType: ['GuardDuty Finding'],
+            },
+        });
 
         // Format the GuardDuty findings emails (rather than just a large splodge of JSON)
-        eventRule.addTarget(new eventTargets.SnsTopic(guardDutyTopic, {
-            message: events.RuleTargetInput.fromText(`WARNING: AWS GuardDuty has discovered a ${events.EventField.fromPath('$.detail.type')} security issue for ${props.environmentName} (${events.EventField.fromPath('$.region')}). Please go to https://${events.EventField.fromPath('$.region')}.console.aws.amazon.com/guardduty/ to find out more details.`)
-        }));
-
+        eventRule.addTarget(
+            new eventTargets.SnsTopic(guardDutyTopic, {
+                message: events.RuleTargetInput.fromText(
+                    `WARNING: AWS GuardDuty has discovered a ${events.EventField.fromPath(
+                        '$.detail.type',
+                    )} security issue for ${props.environmentName} (${events.EventField.fromPath(
+                        '$.region',
+                    )}). Please go to https://${events.EventField.fromPath(
+                        '$.region',
+                    )}.console.aws.amazon.com/guardduty/ to find out more details.`,
+                ),
+            }),
+        );
     }
 }
