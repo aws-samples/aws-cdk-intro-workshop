f3eb6b2b540f458075d37139cfe7b124887f15b2
Stefan Asseg <stefanx@amazon.com>
2023-01-05 03:01:36 -0700

    feat: Add hello-app the consumes the hitcounter construct

---
+++ code/typescript/internal-constructs-workshop/hello-app/bin/cdk-workshop.ts
@@ -0,0 +1,5 @@
+import { App } from 'aws-cdk-lib';
+import { WorkshopPipelineStack } from "../lib/pipeline-stack";
+
+const app = new App();
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
---
+++ code/typescript/internal-constructs-workshop/hello-app/lib/cdk-workshop-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from 'cdkworkshop-lib';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
+export class CdkWorkshopStack extends cdk.Stack {
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    const tv = new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, 'TableViewerUrl', {
+      value: tv.endpoint
+    });
+  }
+}
